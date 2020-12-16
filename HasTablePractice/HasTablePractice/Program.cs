46caffbeaa333a77f08944657dce8cf4015891e2
mh pranta <mhpranta2@gmail.com>
2020-12-16 19:06:03 +0600

    HashTable Practice

---
+++ HasTablePractice/HasTablePractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HasTablePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable table = new Hashtable();
+            table["pranta"] = 18362331;         // Hastable is able to store no inside index of string 
+            table["Maruf"] = 18366181;
+            table['A'] = "HashTable is able to store indexed char";  // Hastable is able to store string inside index of character
+            table[0] = "HashTable is able to store indexed int";                // Hastable is able to store string inside index of integer
+            table[0.5] = "HashTable is able to store indexed float";             // Hastable is able to store string inside index of float 
+            Console.WriteLine(table["pranta"]);
+            Console.WriteLine(table["Maruf"]);
+            Console.WriteLine(table['A']);
+            Console.WriteLine(table[0]);
+            Console.WriteLine(table[0.5]);
+
+
+        }
+    }
+}
